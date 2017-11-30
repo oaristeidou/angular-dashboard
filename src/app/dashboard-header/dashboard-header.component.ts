ea37647e9ef81c643a1559b3236cf4ab83526844
Odyssefs Aristeidou <airoddyisback@gmail.com>
2017-11-30 22:34:48 +0100

    - Add the sections logic for the graph display

--- src/app/dashboard-header/dashboard-header.component.ts
+++ src/app/dashboard-header/dashboard-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard-header',
@@ -7,12 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardHeaderComponent {
 
-  constructor() {
+  constructor(private router: Router) {
 
   }
 
   setGraphs(g1, g2, g3) {
     console.log(g1, g2, g3);
+
+    let sections = "";
+
+    if (g1){
+      sections = "section1:section1";
+    }
+
+    if (g2){
+      if (sections){
+        sections += "//";
+      }
+      sections += "section2:section2";
+    }
+
+    if (g3) {
+      if (sections){
+        sections += "//";
+      }
+      sections += "section3:section3";
+    }
+
+    let url = "/home";
+
+    if (sections) {
+      url += `(${sections})`;
+    }
+
+    console.log(url);
+
+    this.router.navigateByUrl(url);
 
   }
 
